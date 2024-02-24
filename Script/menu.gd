e560e6a4fdeb301c05cd74c69d86ee37916f738b
Manyunidev <isys_admin@gospod.amin>
2024-02-25 02:31:47 +0300

    walls repair

--- Script/menu.gd
+++ Script/menu.gd
@@ -5,9 +5,8 @@ extends Node2D
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scene/level.tscn")
+	get_tree().change_scene_to_file("res://Scene/level_house.tscn")
 
 
 func _on_quit_pressed():
 	get_tree().quit()
-	#chert
